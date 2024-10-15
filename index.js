c6d70c793e803f4540b08d4ec825886f386c0125
yuzalsif <liteit68@gmail.com>
2024-10-15 20:17:47 +0300

    Testing express installation

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) =>
+  res.send("This is a response coming from the  Node API")
+);
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
