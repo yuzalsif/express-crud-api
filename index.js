13c232c5e567d275712e41b473766b763f781382
yuzalsif <liteit68@gmail.com>
2024-10-15 20:33:34 +0300

    Testing connection to the database

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
 const port = process.env.PORT || 3000;
 app.get("/", (req, res) =>
   res.send("This is a response coming from the  Node API")
 );
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+mongoose
+  .connect(
+    "mongodb+srv://yuseifabdillah:ZhD8DD8oo0CdclzG@experimental.32yht.mongodb.net/?retryWrites=true&w=majority&appName=Experimental"
+  )
+  .then(() => console.log("Connected!"))
+  .catch((err) => console.log("Connection to the database failed", err));
