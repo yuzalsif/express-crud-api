a420da2f00e53b3956324b9ede05d9389e945d73
yuzalsif <liteit68@gmail.com>
2024-10-15 20:55:35 +0300

    feat: Add product model

--- index.js
+++ index.js
@@ -6,11 +6,15 @@ const port = process.env.PORT || 3000;
 app.get("/", (req, res) =>
   res.send("This is a response coming from the  Node API")
 );
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 mongoose
   .connect(
     "mongodb+srv://yuseifabdillah:ZhD8DD8oo0CdclzG@experimental.32yht.mongodb.net/?retryWrites=true&w=majority&appName=Experimental"
   )
-  .then(() => console.log("Connected!"))
+  .then(() => {
+    console.log("Connected!");
+    app.listen(port, () =>
+      console.log(`Example app listening on port ${port}!`)
+    );
+  })
   .catch((err) => console.log("Connection to the database failed", err));
---
+++ models/product.model.ts
@@ -0,0 +1,36 @@
+import { timeStamp } from "console";
+
+const mongoose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter product name"],
+    },
+
+    quantity: {
+      type: Number,
+      required: [true, "Please enter product quantity"],
+      default: 0,
+    },
+
+    price: {
+      type: Number,
+      required: [true, "Please enter product price"],
+      default: 0,
+    },
+
+    image: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    timeStamp: true,
+  }
+);
+
+const Product = mongoose.model("Product", ProductSchema);
+
+module.exports = Product;
